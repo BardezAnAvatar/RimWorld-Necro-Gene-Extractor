631636c30154c6bb0c3d45c7f90710f2c92f3486
Erik Frantz <bardez@bardez.net>
2025-11-17 14:29:34 -0600

    Implement IExposable

--- src/NecroGeneExtractor/Settings/NecroGeneExtractorSettings.cs
+++ src/NecroGeneExtractor/Settings/NecroGeneExtractorSettings.cs
@@ -12,25 +12,36 @@ public class NecroGeneExtractorSettings : ModSettings
     public float CostResourceFresh = 5f;
     public float CostTimeFresh = 3f;
 
-    private readonly NecroGeneExtractorTierSettings _settingsTier2 
-        = new(new(5f, 6f), new(false, defaultCostResourceMultiplierRotting, defaultCostTimeMultipierRotting), new(false, defaultCostResourceMultiplierDessicated, defaultCostTimeMultipierDessicated));
-    private readonly NecroGeneExtractorTierSettings _settingsTier3
-        = new(new(4f, 4.5f), new(true, defaultCostResourceMultiplierRotting, defaultCostTimeMultipierRotting), new(false, defaultCostResourceMultiplierDessicated, defaultCostTimeMultipierDessicated));
-    private readonly NecroGeneExtractorTierSettings _settingsTier4
-        = new(new(3f, 2.5f), new(true, defaultCostResourceMultiplierRotting, defaultCostTimeMultipierRotting), new(true, defaultCostResourceMultiplierDessicated, defaultCostTimeMultipierDessicated));
+    private NecroGeneExtractorTierSettings _settingsTier2 
+        = new(
+            new(5f, 6f),
+            new(false, defaultCostResourceMultiplierRotting, defaultCostTimeMultipierRotting),
+            new(false, defaultCostResourceMultiplierDessicated, defaultCostTimeMultipierDessicated));
+    private NecroGeneExtractorTierSettings _settingsTier3
+        = new(
+            new(4f, 4.5f),
+            new(true, defaultCostResourceMultiplierRotting, defaultCostTimeMultipierRotting),
+            new(false, defaultCostResourceMultiplierDessicated, defaultCostTimeMultipierDessicated));
+    private NecroGeneExtractorTierSettings _settingsTier4
+        = new(
+            new(3f, 2.5f),
+            new(true, defaultCostResourceMultiplierRotting, defaultCostTimeMultipierRotting),
+            new(true, defaultCostResourceMultiplierDessicated, defaultCostTimeMultipierDessicated));
 
     public NecroGeneExtractorTierSettings SettingsTier2 => _settingsTier2;
+
     public NecroGeneExtractorTierSettings SettingsTier3 => _settingsTier3;
+
     public NecroGeneExtractorTierSettings SettingsTier4 => _settingsTier4;
 
     /// <summary>
     /// The part that writes our settings to a file. Note that saving is by ref. Labels cannot use spaces (xml tags)
-    /// </summary>
+    /// </summary>, 
     public override void ExposeData()
     {
         base.ExposeData();
-        _settingsTier2.ExposeData(nameof(SettingsTier2));
-        _settingsTier2.ExposeData(nameof(SettingsTier3));
-        _settingsTier2.ExposeData(nameof(SettingsTier4));
+        Scribe_Values.Look(ref _settingsTier2, nameof(SettingsTier2));
+        Scribe_Values.Look(ref _settingsTier3, nameof(SettingsTier3));
+        Scribe_Values.Look(ref _settingsTier4, nameof(SettingsTier4));
     }
 }
--- src/NecroGeneExtractor/Settings/NecroGeneExtractorTierSettings.cs
+++ src/NecroGeneExtractor/Settings/NecroGeneExtractorTierSettings.cs
@@ -2,16 +2,20 @@
 
 namespace Bardez.Biotech.NecroGeneExtractor.Settings;
 
-public class NecroGeneExtractorTierSettings(NecroGeneExtractorCorpseSettingsFresh fresh, NecroGeneExtractorCorpseSettingsNonFresh rotting, NecroGeneExtractorCorpseSettingsNonFresh dessicated)
+public class NecroGeneExtractorTierSettings(NecroGeneExtractorCorpseSettingsFresh fresh,
+    NecroGeneExtractorCorpseSettingsNonFresh rotting,
+    NecroGeneExtractorCorpseSettingsNonFresh dessicated) : IExposable
 {
     public NecroGeneExtractorCorpseSettingsFresh Fresh => fresh;
+
     public NecroGeneExtractorCorpseSettingsNonFresh Rotting => rotting;
+
     public NecroGeneExtractorCorpseSettingsNonFresh Dessicated => dessicated;
 
-    public void ExposeData(string prefix)
+    public void ExposeData()
     {
-        Fresh.ExposeData($"{prefix}.{nameof(Fresh)}");
-        Rotting.ExposeData($"{prefix}.{nameof(Rotting)}");
-        Dessicated.ExposeData($"{prefix}.{nameof(Dessicated)}");
+        Scribe_Values.Look(ref fresh, nameof(Fresh));
+        Scribe_Values.Look(ref rotting, nameof(Rotting));
+        Scribe_Values.Look(ref dessicated, nameof(Dessicated));
     }
 }
