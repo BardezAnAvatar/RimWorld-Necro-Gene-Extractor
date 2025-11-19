238c5963bc42bc373fd8731c330bb70517c7c23e
Erik Frantz <bardez@bardez.net>
2025-11-19 11:49:07 -0600

    Minor detail implementation

---
+++ src/NecroGeneExtractor/Buildings/NecroGeneExtractorTier2.cs
@@ -0,0 +1,10 @@
+﻿using Bardez.Biotech.NecroGeneExtractor.Settings.Tiers;
+using Verse;
+
+namespace Bardez.Biotech.NecroGeneExtractor.Buildings;
+
+[StaticConstructorOnStartup]
+public class NecroGeneExtractorTier2 : NecroGeneExtractorBase
+{
+    protected override TierSettings TierSettings => NecroSettings.SettingsTier2;
+}
