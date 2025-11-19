63fb1e209af1457a0a9f4bcfdd7ca48de2437a82
Erik Frantz <bardez@bardez.net>
2025-11-18 21:12:06 -0600

    Re-arranging and setting up new default constants

--- src/NecroGeneExtractor/Settings/DefaultSettings.cs
+++ src/NecroGeneExtractor/Settings/DefaultSettings.cs
@@ -2,23 +2,44 @@
 
 internal static class DefaultSettings
 {
-    public const float TIER2_FRESH_RESOURCE = 5f;
-    public const float TIER2_FRESH_TIME = 6f;
-    public const bool TIER2_ROTTING_ACCEPT = false;
-    public const bool TIER2_DESSICATED_ACCEPT = false;
+    /// <summary>Amount of Neutroamine consumed per hour</summary>
+    public const float FRESH_RESOURCE = 5f;
 
-    public const float TIER3_FRESH_RESOURCE = 4f;
-    public const float TIER3_FRESH_TIME = 4.5f;
-    public const bool TIER3_ROTTING_ACCEPT = true;
-    public const bool TIER3_DESSICATED_ACCEPT = false;
+    /// <summary>Processing time of a single cycle of the corpse gene extraction, in hours</summary>
+    public const float FRESH_TIME = 24f;
 
-    public const float TIER4_FRESH_RESOURCE = 3f;
-    public const float TIER4_FRESH_TIME = 2.5f;
-    public const bool TIER4_ROTTING_ACCEPT = true;
-    public const bool TIER4_DESSICATED_ACCEPT = true;
+    public const float MULTIPLIER_RESOURCE_ROTTING = 3f;
+    public const float MULTIPLIER_RESOURCE_DESSICATED = 5f;
+    public const float MULTIPLIER_TIME_ROTTING = 6f;
+    public const float MULTIPLIER_TIME_DESSICATED = 10f;
 
-    public const float ROTTING_RESOURCE = 3f;
-    public const float DESSICATED_RESOURCE = 5f;
-    public const float ROTTING_TIME = 6f;
-    public const float DESSICATED_TIME = 10f;
+    public static class Tier2
+    {
+        public const bool ACCEPT_ROTTING = false;
+        public const bool ACCEPT_DESSICATED = false;
+        public const float MULTIPLIER_TIME = 1f; //base speed
+        public const float MULTIPLIER_RESOURCE = 1f; //base consumption
+        public const float MULTIPLIER_OVERDRIVE_TIME = 3f; //base overdrive speed-up
+        public const float MULTIPLIER_OVERDRIVE_RESOURCE = 2f; //base overdrive consumption
+    }
+
+    public static class Tier3
+    {
+        public const bool ACCEPT_ROTTING = true;
+        public const bool ACCEPT_DESSICATED = false;
+        public const float MULTIPLIER_TIME = 0.8f; //125% faster
+        public const float MULTIPLIER_RESOURCE = 2f; //200% consumption
+        public const float MULTIPLIER_OVERDRIVE_TIME = 3f; //overdrive speed-up
+        public const float MULTIPLIER_OVERDRIVE_RESOURCE = 2f; //overdrive consumption
+    }
+
+    public static class Tier4
+    {
+        public const bool ACCEPT_ROTTING = true;
+        public const bool ACCEPT_DESSICATED = true;
+        public const float MULTIPLIER_TIME = 0.4f; //250% faster
+        public const float MULTIPLIER_RESOURCE = 4f; //400% consumption
+        public const float MULTIPLIER_OVERDRIVE_TIME = 3f; //base overdrive speed-up
+        public const float MULTIPLIER_OVERDRIVE_RESOURCE = 2f; //base overdrive consumption
+    }
 }
