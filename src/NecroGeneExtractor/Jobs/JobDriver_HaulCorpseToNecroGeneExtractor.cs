d7a0f46802620626f0517334e7e512d05eb943a5
Erik Frantz <bardez@bardez.net>
2025-11-20 09:12:30 -0600

    Adding in a new Job type for hauling to the vat

---
+++ src/NecroGeneExtractor/Jobs/JobDriver_HaulCorpseToNecroGeneExtractor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Bardez.Biotech.NecroGeneExtractor.Buildings;
+using Verse;
+using Verse.AI;
+
+namespace Bardez.Biotech.NecroGeneExtractor.Jobs;
+
+public abstract class JobDriver_HaulCorpseToNecroGeneExtractor : JobDriver
+{
+    private int FillDuration => 300;
+
+    protected virtual NecroGeneExtractorBase GeneVat
+    {
+        get
+        {
+            LocalTargetInfo target = job.GetTarget(TargetIndex.A);
+            return target.Thing as NecroGeneExtractorBase;
+        }
+    }
+
+    protected Corpse TargetCorpse
+    {
+        get
+        {
+            LocalTargetInfo target = job.GetTarget(TargetIndex.B);
+            return target.Thing as Corpse;
+        }
+    }
+
+    public override bool TryMakePreToilReservations(bool errorOnFailed) 
+        => ReservationUtility.Reserve(this.pawn, this.GeneVat, this.job, 1, -1, null, errorOnFailed, false)
+        && ReservationUtility.Reserve(this.pawn, this.TargetCorpse, this.job, 1, -1, null, errorOnFailed, false);
+
+    protected override IEnumerable<Toil> MakeNewToils()
+    {
+        JobDriver_HaulCorpseToNecroGeneExtractor necroGeneExtractorJob = this;
+        ToilFailConditions.FailOnDespawnedNullOrForbidden<JobDriver_HaulCorpseToNecroGeneExtractor>(this, TargetIndex.A);
+        necroGeneExtractorJob.AddEndCondition(GetJobStatus);
+
+        yield return ToilFailConditions.FailOnSomeonePhysicallyInteracting(
+            ToilFailConditions.FailOnDespawnedNullOrForbidden(
+                Toils_Goto.GotoThing(TargetIndex.B, PathEndMode.InteractionCell, false), TargetIndex.B),
+            TargetIndex.B);
+
+        yield return ToilFailConditions.FailOnDestroyedNullOrForbidden(
+            Toils_Haul.StartCarryThing(TargetIndex.B, false, true, false, true, false),
+            TargetIndex.B);
+
+        yield return Toils_Goto.GotoThing(TargetIndex.A, PathEndMode.InteractionCell, false);
+
+        yield return ToilEffects.WithProgressBarToilDelay(
+            ToilFailConditions.FailOnCannotTouch(
+                ToilFailConditions.FailOnDestroyedNullOrForbidden(
+                    ToilFailConditions.FailOnDestroyedNullOrForbidden(
+                        Toils_General.Wait(necroGeneExtractorJob.FillDuration, TargetIndex.None),
+                        TargetIndex.B),
+                    TargetIndex.A),
+                TargetIndex.A, PathEndMode.Touch),
+            TargetIndex.A, false, -0.5f);
+
+        yield return new Toil()
+        {
+            initAction = new Action(() =>
+            {
+                this.GeneVat.TryAcceptCorpse(TargetCorpse);
+                this.pawn.jobs.EndCurrentJob(JobCondition.Succeeded, true, true);
+                //TargetCorpse.DeSpawn(DestroyMode.Vanish); handled in building method
+            }),
+            defaultCompleteMode = ToilCompleteMode.Instant,
+        };
+    }
+
+    private JobCondition GetJobStatus()
+        => this.GeneVat.Corpse != null 
+            ? JobCondition.Succeeded
+            : JobCondition.Ongoing;
+}
