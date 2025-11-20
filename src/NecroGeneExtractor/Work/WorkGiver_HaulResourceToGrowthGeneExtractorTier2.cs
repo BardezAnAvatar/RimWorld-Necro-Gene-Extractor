71661d6c30594be2c134f2a3f882ac09883ca952
Erik Frantz <bardez@bardez.net>
2025-11-19 23:11:15 -0600

    Add in defs specific to ExtractorTier2

---
+++ src/NecroGeneExtractor/Work/WorkGiver_HaulResourceToGrowthGeneExtractorTier2.cs
@@ -0,0 +1,9 @@
+﻿using Bardez.Biotech.NecroGeneExtractor.Defs;
+using Verse;
+
+namespace Bardez.Biotech.NecroGeneExtractor.Work;
+
+public class WorkGiver_HaulResourceToGrowthGeneExtractorTier2 : WorkGiver_HaulResourceToGrowthGeneExtractorBase
+{
+    public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForDef(NecroGeneExtractor_DefsOf.NecroGeneExtractorTier2);
+}
