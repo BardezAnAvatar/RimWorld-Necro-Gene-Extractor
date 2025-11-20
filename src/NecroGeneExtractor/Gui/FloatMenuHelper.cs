48e46063ea7359e5c27e7ba27f40619fb788947e
Erik Frantz <bardez@bardez.net>
2025-11-19 21:13:15 -0600

    Adding in a FloatMenu helper class for building out the list of corpses available

---
+++ src/NecroGeneExtractor/Gui/FloatMenuHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Bardez.Biotech.NecroGeneExtractor.Gui;
+
+public static class FloatMenuHelper
+{
+    public static void BuildFloatMenuAvailableCorpses(Map map, Func<Corpse, AcceptanceReport> canAcceptCorpse, Action<Corpse> selectCorpse)
+    {
+        List<FloatMenuOption> list = [];
+        var corpses = map.listerThings.AllThings
+            .OfType<Corpse>();
+
+        foreach (Corpse corpse in corpses)
+        {
+            if (corpse.InnerPawn.genes != null)
+            {
+                AcceptanceReport acceptanceReport = canAcceptCorpse(corpse);
+                string text = corpse.LabelShortCap + ", " + corpse.InnerPawn.genes.XenotypeLabelCap;
+                if (!acceptanceReport.Accepted)
+                {
+                    if (!acceptanceReport.Reason.NullOrEmpty())
+                    {
+                        list.Add(new FloatMenuOption(text + ": " + acceptanceReport.Reason, null, corpse, Color.white));
+                    }
+                }
+                else
+                {
+                    list.Add(new FloatMenuOption(text, () => selectCorpse(corpse), corpse, Color.white));
+                }
+            }
+        }
+
+        if (!list.Any())
+        {
+            list.Add(new FloatMenuOption("NoExtractablePawns".Translate(), null));
+        }
+
+        Find.WindowStack.Add(new FloatMenu(list));
+    }
+}
