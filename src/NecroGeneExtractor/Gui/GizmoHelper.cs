0699be79ac9dbcfb4580bbe0e7fe04f712fdf513
Erik Frantz <bardez@bardez.net>
2025-11-19 21:11:10 -0600

    Add my own Gizmo helper for building out the corpse selection Gizmo

---
+++ src/NecroGeneExtractor/Gui/GizmoHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using GeneExtractorTiers.Extractors;
+using Verse;
+
+namespace Bardez.Biotech.NecroGeneExtractor.Gui;
+
+public static class GizmoHelper
+{
+    public static Command_Action BuildGizmoInsertCorpse(Action corpseSelector, bool powered)
+    {
+        var insertPerson = new Command_Action()
+        {
+            defaultLabel = "NGET_InsertCorpse".Translate() + "...",
+            defaultDesc = "NGET_InsertCorpseTooltip".Translate(),
+            icon = Textures.InsertPawn,
+            action = corpseSelector,
+        };
+
+        if (!powered)
+        {
+            insertPerson.Disable("NoPower".Translate().CapitalizeFirst());
+        }
+
+        return insertPerson;
+    }
+}
