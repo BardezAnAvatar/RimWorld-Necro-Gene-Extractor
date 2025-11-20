59278b8abba4a053ea7d2dfea9c54b36e5ef8c8d
Erik Frantz <bardez@bardez.net>
2025-11-19 23:10:21 -0600

    Add in a Defs class for things to reference directly

---
+++ src/NecroGeneExtractor/Defs/NecroGeneExtractor_DefsOf.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Bardez.Biotech.NecroGeneExtractor.Defs;
+
+[DefOf]
+public static class NecroGeneExtractor_DefsOf
+{
+    public static ThingDef NecroGeneExtractorTier2;
+    public static ThingDef Neutroamine;
+}
