baf92fc7b8ca28467ecb62c43c3983bd818cd773
Erik Frantz <bardez@bardez.net>
2025-11-20 01:01:06 -0600

    Add in a ton of debugging messages for why my job isn't working

--- src/NecroGeneExtractor/Work/WorkGiver_CarryCorpseToNecroGeneExtractorBase.cs
+++ src/NecroGeneExtractor/Work/WorkGiver_CarryCorpseToNecroGeneExtractorBase.cs
@@ -1,5 +1,6 @@
 ﻿using Bardez.Biotech.NecroGeneExtractor.Buildings;
 using Bardez.Biotech.NecroGeneExtractor.Defs;
+using Bardez.Biotech.NecroGeneExtractor.Utilities;
 using RimWorld;
 using Verse;
 using Verse.AI;
@@ -25,11 +26,14 @@ public abstract class WorkGiver_CarryCorpseToNecroGeneExtractorBase : WorkGiver_
             return false;
         }
 
+        DebugMessaging.DebugMessage("Not burning.");
+
         if (t is not NecroGeneExtractorBase geneVat)
         {
             return false;
         }
-        Corpse selectedCorpse = geneVat.TargetedCorpse;
+
+        DebugMessaging.DebugMessage("Is a gene vat.");
 
         // filled already -- no way to fill this
         if (geneVat.Corpse != null)
@@ -37,14 +41,36 @@ public abstract class WorkGiver_CarryCorpseToNecroGeneExtractorBase : WorkGiver_
             return false;
         }
 
-        if (selectedCorpse == null || selectedCorpse.Map != pawn.Map || !geneVat.CanAcceptCorpse(selectedCorpse))
+        DebugMessaging.DebugMessage("gene vat is not filled.");
+
+        Corpse selectedCorpse = geneVat.TargetedCorpse;
+        if (selectedCorpse == null)
         {
             return false;
         }
 
+        DebugMessaging.DebugMessage($"Targeted Corpse is not null; name is `{selectedCorpse.InnerPawn.Name}`.");
+
+        if (selectedCorpse.Map != pawn.Map)
+        {
+            return false;
+        }
+        DebugMessaging.DebugMessage("Corpse is on the same map.");
+
+        if (!geneVat.CanAcceptCorpse(selectedCorpse))
+        {
+            return false;
+        }
+        DebugMessaging.DebugMessage($"Gene vat can accept `{selectedCorpse.InnerPawn.Name}`.");
+
+        DebugMessaging.DebugMessage($"Work Type: `{def?.workType}`.");
+
+
         //the actual job
         if (def.workType != null && pawn.WorkTypeIsDisabled(def.workType))
         {
+            DebugMessaging.DebugMessage($"Pawn can do work type.");
+
             return pawn.CanReserveAndReach(selectedCorpse, PathEndMode.OnCell, Danger.Deadly, 1, -1, null, forced);
         }
 
