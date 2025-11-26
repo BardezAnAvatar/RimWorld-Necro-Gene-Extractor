081510b5ab5fce4a55fc06d41a392582853c09ca
Erik Frantz <bardez@bardez.net>
2025-11-25 22:49:52 -0600

    Migrating over the Textures class that I extracted from `GeneExtractorTiers`

---
+++ src/GeneExtractorTiers/Gui/Textures.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace Bardez.GeneExtractorTiers
+{
+    public static class Textures
+    {
+        public static readonly Texture2D CancelIcon = ContentFinder<Texture2D>.Get("UI/Designators/Cancel");
+        public static readonly Texture2D ActivateOverdrive = ContentFinder<Texture2D>.Get("GET_OverdriveOn");
+        public static readonly Texture2D CancelOverdrive = ContentFinder<Texture2D>.Get("GET_OverdriveOff");
+        public static readonly Texture2D TargetGeneIcon = ContentFinder<Texture2D>.Get("GET_TargetGene");
+        public static readonly Texture2D InsertPawn = ContentFinder<Texture2D>.Get("UI/Gizmos/InsertPawn");
+    }
+}
