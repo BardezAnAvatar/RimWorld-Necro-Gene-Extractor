7d05e81be271a433f1ba6c6d56c690d6dfeeed69
Erik Frantz <bardez@bardez.net>
2025-12-01 23:16:39 -0600

    Utilize the built-in ticks to time conversion method

--- src/GeneExtractorTiers/Buildings/GeneExtractorBase.cs
+++ src/GeneExtractorTiers/Buildings/GeneExtractorBase.cs
@@ -450,7 +450,7 @@ public abstract class GeneExtractorBase : Building_Enterable, IThingHolderWithDr
     {
         stringBuilder
             .AppendLineIfNotEmpty()
-            .Append($"{"TimeLeft".Translate().CapitalizeFirst()}: {(TicksRemaining / WorkPerTick / GenDate.TicksPerHour ) + 1} {"HoursLower".Translate()}");
+            .Append($"{"TimeLeft".Translate().CapitalizeFirst()}: {Convert.ToInt32(TicksRemaining / WorkPerTick / GenDate.TicksPerHour).ToStringTicksToPeriod()}");
     }
 
     protected virtual void InspectStringAddPawn(StringBuilder stringBuilder)
