9fc7e01efca3843855566d8555adf479ab2aa5ef
Erik Frantz <bardez@bardez.net>
2025-12-02 09:33:30 -0600

    Sub-class the CompRefuelable and its Properties to override the Excessively long InspectString

---
+++ src/NecroGeneExtractor/Comps/CompRefuelableNecroVat.cs
@@ -0,0 +1,44 @@
+﻿using Bardez.Biotech.NecroGeneExtractor.Buildings;
+using RimWorld;
+using Verse;
+
+namespace Bardez.Biotech.NecroGeneExtractor.Comps;
+
+/// <summary>Subclass of <see cref="CompRefuelable" /> that prints the InspectString additions differently</summary>
+public class CompRefuelableNecroVat : CompRefuelable
+{
+    public override string CompInspectStringExtra()
+    {
+        string text = Props.FuelLabel + ": " + Fuel.ToStringDecimalIfSmall() + " / ";
+        if (Props.targetFuelLevelConfigurable)
+        {
+            text += "ConfiguredTargetFuelLevel".Translate(TargetFuelLevel.ToStringDecimalIfSmall())
+                + " [" + Props.fuelCapacity.ToStringDecimalIfSmall() + "]";
+        }
+        else
+        {
+            text += Props.fuelCapacity.ToStringDecimalIfSmall();
+        }
+
+        if (!Props.consumeFuelOnlyWhenUsed && HasFuel)
+        {
+            text += " ";
+            int numTicks = (int)(Fuel / Props.fuelConsumptionRate * 60000f);
+
+            if (parent is NecroGeneExtractor_Base necroVat && necroVat.Working)
+            {
+                var hourlyNeutroRate = necroVat.NeutroConsumedPerHour.ToString("F2");
+                text += "(-" + "PerHour".Translate(hourlyNeutroRate) + ") ";
+            }
+            text += "NGET_RemainingFuelTime".Translate().Formatted(numTicks.ToStringTicksToPeriod());
+        }
+
+        if (!HasFuel && !Props.outOfFuelMessage.NullOrEmpty())
+        {
+            string arg = Props.outOfFuelMessage;
+            text += $"\n{arg} ({GetFuelCountToFullyRefuel()}x {Props.fuelFilter.AnyAllowedDef.label})";
+        }
+
+        return text;
+    }
+}
---
+++ src/NecroGeneExtractor/Comps/Properties/CompProperties_RefuelableNecroVat.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+using RimWorld;
+
+namespace Bardez.Biotech.NecroGeneExtractor.Comps.Properties;
+
+public class CompProperties_RefuelableNecroVat : CompProperties_Refuelable
+{
+    public CompProperties_RefuelableNecroVat()
+    {
+        this.compClass = typeof(CompRefuelableNecroVat);
+    }
+}
