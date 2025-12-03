a5a883c511c3adb3f658244f6dfcaa4811aad866
Erik Frantz <bardez@bardez.net>
2025-12-02 20:33:54 -0600

    Set the resource consumption multiplier default to 3

--- src/NecroGeneExtractor/Settings/DefaultSettings.cs
+++ src/NecroGeneExtractor/Settings/DefaultSettings.cs
@@ -20,7 +20,7 @@ internal static class DefaultSettings
         public const float MULTIPLIER_TIME = 1f; //base speed
         public const float MULTIPLIER_RESOURCE = 1f; //base consumption
         public const float MULTIPLIER_OVERDRIVE_TIME = 2f; //base overdrive speed-up
-        public const float MULTIPLIER_OVERDRIVE_RESOURCE = 2f; //base overdrive consumption
+        public const float MULTIPLIER_OVERDRIVE_RESOURCE = 3f; //base overdrive consumption
     }
 
     public static class Tier3
@@ -30,7 +30,7 @@ internal static class DefaultSettings
         public const float MULTIPLIER_TIME = 0.8f; //125% faster
         public const float MULTIPLIER_RESOURCE = 2f; //200% consumption
         public const float MULTIPLIER_OVERDRIVE_TIME = 2f; //overdrive speed-up
-        public const float MULTIPLIER_OVERDRIVE_RESOURCE = 2f; //overdrive consumption
+        public const float MULTIPLIER_OVERDRIVE_RESOURCE = 3f; //overdrive consumption
     }
 
     public static class Tier4
@@ -40,6 +40,6 @@ internal static class DefaultSettings
         public const float MULTIPLIER_TIME = 0.4f; //250% faster
         public const float MULTIPLIER_RESOURCE = 4f; //400% consumption
         public const float MULTIPLIER_OVERDRIVE_TIME = 2f; //base overdrive speed-up
-        public const float MULTIPLIER_OVERDRIVE_RESOURCE = 2f; //base overdrive consumption
+        public const float MULTIPLIER_OVERDRIVE_RESOURCE = 3f; //base overdrive consumption
     }
 }
